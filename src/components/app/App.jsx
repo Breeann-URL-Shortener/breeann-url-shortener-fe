9711fa0cbd6954a29030d3f39c9699520ebbcb75
Breeann Bolinsky <bolinskybm10@gmail.com>
2021-05-30 16:02:31 -0700

    added router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import ShortenContainer from '../../containers/ShortenContainer';
 
 export default function App() {
   // Take in container
-  return <ShortenContainer />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={ShortenContainer}/>
+
+      </Switch>
+    </Router> 
+    
+  );
 }
